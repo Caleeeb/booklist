c8828296381f22dafb0cf0667f1358b380d6af60
Caleb Day <day.caleb@gmail.com>
2023-03-01 23:30:56 -0500

    add form and button with state and handlers, styling next

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title)
+    console.log('Need to add book with:', title);
   };
 
-  return 
+  return (
   <div>
-      <BookCreate onCreate={createBook} />
+    <BookCreate onCreate={createBook} />
   </div>
+  );
 }
 
 export default App;
